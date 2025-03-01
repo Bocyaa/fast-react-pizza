c4659e6b048da498efd520dc1c757b8f65892b07
Abdubositkhon Khabirov <xabirovbob@gmail.com>
2025-03-01 20:32:27 +0100

    Style with Tailwind CSS

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const navigate = useNavigate();
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        placeholder='Search order #'
+        placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="focus::ring-yellow-500 w-28 rounded-full bg-yellow-200 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
@@ -19,7 +20,7 @@ function SearchOrder() {
     e.preventDefault();
     if (!query) return;
     navigate(`/order/${query}`);
-    setQuery('');
+    setQuery("");
   }
 }
 
